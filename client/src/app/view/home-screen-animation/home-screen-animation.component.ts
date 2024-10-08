0b47ba0a24a0c193d428f5b33ca093915f47640c
Mariangel <rondanmariangel@gmail.com>
2024-10-08 12:56:13 -0300

    home screen animation

--- client/src/app/view/home-screen-animation/home-screen-animation.component.ts
+++ client/src/app/view/home-screen-animation/home-screen-animation.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-home-screen-animation',
   standalone: true,
   imports: [],
   templateUrl: './home-screen-animation.component.html',
-  styleUrl: './home-screen-animation.component.css'
+  styleUrl: './home-screen-animation.component.css',
 })
 export class HomeScreenAnimationComponent {
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {
+    // Redirige después de 5 segundos (5000 ms), puedes ajustar el tiempo según el tiempo de animación.
+    setTimeout(() => {
+      this.router.navigate(['/home']);
+    }, 4000); // Cambia 5000 por el tiempo que desees.
+  }
 }
