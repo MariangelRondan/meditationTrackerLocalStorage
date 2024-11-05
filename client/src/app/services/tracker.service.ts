6e89c337e41205a4f98545d23eb8ad1de0280687
Mariangel <rondanmariangel@gmail.com>
2024-11-05 20:44:44 -0300

    render url

--- client/src/app/services/tracker.service.ts
+++ client/src/app/services/tracker.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class TrackerService {
-  API_URL = 'http://localhost:3000/tracker';
+  API_URL = 'https://meditaitiontracker.onrender.com';
 
   constructor(private http: HttpClient) {}
 
